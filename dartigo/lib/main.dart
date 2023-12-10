77fca8d584570650e6e9bf4df17c73745eb2b994
SQL Srijon <116680350+srijon57@users.noreply.github.com>
2023-12-11 00:36:46 +0600

    point 2

--- dartigo/lib/main.dart
+++ dartigo/lib/main.dart
@@ -2,7 +2,10 @@ import 'package:firstly/page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: (MyApp())));
+  runApp(MaterialApp(
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
 class MyApp extends StatelessWidget {
